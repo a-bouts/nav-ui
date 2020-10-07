c6ec8be8180d5e2804201d1ff6b5d24facb67f8b
Arnaud Bouts <arnaud.bouts@orange.com>
2020-10-07 11:42:17 +0200

    improve door display

--- src/components/Race.vue
+++ src/components/Race.vue
@@ -24,6 +24,11 @@ export default {
   },
   watch: {
     current: function() {
+      this.drawRace()
+    }
+  },
+  methods: {
+    drawRace() {
       var it = this
 
       this.validated = JSON.parse(localStorage.getItem("_validated_" + this.current.id))
@@ -56,30 +61,15 @@ export default {
         var markerIcon = L.ExtraMarkers.icon({icon: 'fa-number', number: door.name, shape: 'penta', markerColor: door.validated === true ? 'green-light' : 'yellow'});
 
         L.marker([door.latlons[0].lat, door.latlons[0].lon], {door: door, icon: markerIcon}).on('click', function() {
-            this.options.door.validated = !this.options.door.validated;
-            this.setIcon(L.ExtraMarkers.icon({icon: 'fa-number', number: this.options.door.name, shape: 'penta', markerColor: this.options.door.validated === true ? 'green-light' : 'yellow'}));
-
-            if(this.options.door.validated === true) {
-              if(!it.validated) it.validated = []
-              it.validated.push(this.options.door.name);
-            } else {
-              it.validated.splice(it.validated.indexOf(this.options.door.name), 1);
-            }
-            localStorage.setItem("_validated_" + it.current.id, JSON.stringify(it.validated))
-
-            for(var i in it.races[it.current.id].waypoints) {
-              var door = it.races[it.current.id].waypoints[i];
-              if(!door.validated) {
-                it.$emit('nextdoor', door)
-                break
-              }
-            }
-
+            it.validateBuoy(this)
         }).addTo(this.raceLayer);
 
         if(door.latlons.length > 1) {
-          var markerIconTribord = L.ExtraMarkers.icon({icon: 'fa-number', number: door.name, shape: 'star', markerColor: door.validated === true ? 'green-light' : 'yellow'});
-          L.marker([door.latlons[1].lat, door.latlons[1].lon], {door: door, icon: markerIconTribord}).addTo(this.raceLayer)
+          var markerIconTribord = L.ExtraMarkers.icon({icon: 'fa-number', number: door.name, shape: 'penta', markerColor: door.validated === true ? 'green-light' : 'orange'});
+          L.marker([door.latlons[1].lat, door.latlons[1].lon], {door: door, icon: markerIconTribord}).on('click', function() {
+              it.validateBuoy(this)
+          }).addTo(this.raceLayer)
+          L.polyline([[door.latlons[0].lat, door.latlons[0].lon], [door.latlons[1].lat, door.latlons[1].lon]], {color: 'red', dashArray: '4, 8', dashOffset: '0', weight: 1}).addTo(this.raceLayer);
         }
       }
 
@@ -90,6 +80,27 @@ export default {
           break
         }
       }
+    },
+    validateBuoy(marker) {
+      marker.options.door.validated = !marker.options.door.validated;
+
+      if(marker.options.door.validated === true) {
+        if(!this.validated) this.validated = []
+        this.validated.push(marker.options.door.name);
+      } else {
+        this.validated.splice(this.validated.indexOf(marker.options.door.name), 1);
+      }
+      localStorage.setItem("_validated_" + this.current.id, JSON.stringify(this.validated))
+
+      for(var i in this.races[this.current.id].waypoints) {
+        var door = this.races[this.current.id].waypoints[i];
+        if(!door.validated) {
+          this.$emit('nextdoor', door)
+          break
+        }
+      }
+
+      this.drawRace()
     }
   }
 }
