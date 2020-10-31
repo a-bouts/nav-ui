cd3402156382bc528343051a3b1d1a2e377dbd07
ArnoBouts <arnaud.bouts@gmail.com>
2020-10-31 15:57:13 +0100

    draw geodesic on all race

--- src/components/Geodesic.vue
+++ src/components/Geodesic.vue
@@ -5,41 +5,63 @@
 <script>
 import L from 'leaflet'
 import 'leaflet.geodesic'
+import {EventBus} from '../event-bus.js';
 
 export default {
   name: 'Geodesic',
   props: {
     map: Object,
-    from: Object,
-    to: Object
+    from: Object
   },
   data: function() {
     return {
-      geodesic: null
+      layer: null,
+      buoys: []
     }
   },
   mounted: function() {
+    this.layer = L.layerGroup().addTo(this.map)
+    EventBus.$on('buoys', this.onBuoys)
   },
   methods: {
     convertDMSToDD: function(coord) {
       return Number(coord.p) * (Number(coord.d) + Number(coord.m)/60 + Number(coord.s)/3600);
     },
+    onBuoys: function(buoys) {
+      this.buoys = buoys
+      this.setGeodesic()
+    },
     setGeodesic: function() {
-      if(!this.from || !this.to) return
-      const from = new L.LatLng(this.convertDMSToDD(this.from.lat), this.convertDMSToDD(this.from.lng))
-      const to = new L.LatLng(this.to.latlons[0].lat, this.to.latlons[0].lon)
-      if(this.geodesic) {
-        this.geodesic.setLatLngs([from, to])
-      } else {
-        this.geodesic = L.geodesic([from, to], {weight: 1, opacity: 0.7}).addTo(this.map)
-      }
+      this.layer.clearLayers()
+
+      if(!this.from) return
+
+      var from = {lat: this.convertDMSToDD(this.from.lat), lon: this.convertDMSToDD(this.from.lng), wrap: 0}
+      
+      this.buoys.forEach(b => {
+        if(b.type != "START" && !b.validated) {
+          var to = b.latlons[0]
+          if (b.latlons.length > 1) {
+            to = {
+              lat : (b.latlons[0].lat + b.latlons[1].lat) / 2,
+              lon : (b.latlons[0].lon + b.latlons[1].lon) / 2
+            }
+          }
+          to.wrap = b.wrap || 0
+          this.addGeodesic(from, to)
+          from = to
+        }
+      })
+    },
+    addGeodesic: function(from, to) {
+      console.log("wrap", from.wrap, to.wrap)
+      const fromLatLng = new L.LatLng(from.lat, from.lon + from.wrap * 360)
+      const toLatLng = new L.LatLng(to.lat, to.lon + to.wrap * 360)
+      L.geodesic([fromLatLng, toLatLng], {wrap: false, weight: 1, opacity: 0.7}).addTo(this.layer)
     }
   },
   watch: {
     from: function() {
-      this.setGeodesic()
-    },
-    to: function() {
       this.setGeodesic()
     }
   }
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,7 +8,7 @@
       <SideBar ref="sidebar" v-if="map != null" v-bind:boat="boat" v-bind:debug="debug" v-bind:map="map" v-bind:races="races" v-bind:loading="loading" v-bind:position="current.position" v-on:configure="configure" v-on:center="center" v-on:run="go" v-on:show-tooltip="showTooltip" v-on:error="error"></SideBar>
     </div>
     <Graticule v-if="map != null" v-bind:map="map"></Graticule>
-    <Geodesic ref="geodesic" v-if="map != null" v-bind:from="current.position" v-bind:to="nextDoor" v-bind:map="map"></Geodesic>
+    <Geodesic ref="geodesic" v-if="map != null" v-bind:from="current.position" v-bind:map="map"></Geodesic>
     <Race v-if="map != null" v-bind:boat="boat" v-bind:map="map" v-bind:races="races" v-bind:current="current" v-on:nextdoor="onNextDoor"></Race>
     <Route ref="route" v-if="map != null" v-bind:debug="debug" v-bind:boat="boat" v-bind:map="map" v-bind:races="races" v-bind:layerControl="layerControl" v-bind:current="current" v-on:loading="onLoading" v-on:error="error" v-on:select="selectPoint"></Route>
     <BoatLines ref="boatlines" v-if="map != null" v-bind:map="map" v-bind:races="races" v-bind:layerControl="layerControl" v-bind:current="current"></BoatLines>
