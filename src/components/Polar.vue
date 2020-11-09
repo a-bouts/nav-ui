bb45351ef1d3b10772b84a1113dcb50c4abf88a6
ArnoBouts <arnaud.bouts@gmail.com>
2020-11-09 08:50:53 +0100

    fix sail name

--- src/components/Polar.vue
+++ src/components/Polar.vue
@@ -146,13 +146,13 @@ export default {
         var maxS = null
         this.polar.sail.forEach((sail) => {
 
-          if((sail.name == "LIGHT_JIB" || sail.name == "LIGHT_GNK") && (this.current.sails & 1) != 1) {
+          if((sail.name == "LightJib" || sail.name == "LightGnk") && (this.current.sails & 1) != 1) {
             return
           }
-          if((sail.name == "STAYSAIL" || sail.name == "HEAVY_GNK") && (this.current.sails & 4) != 4) {
+          if((sail.name == "Staysail" || sail.name == "HeavyGnk") && (this.current.sails & 4) != 4) {
             return
           }
-          if(sail.name == "CODE_0" && (this.current.sails & 2) != 2) {
+          if(sail.name == "Code0" && (this.current.sails & 2) != 2) {
             return
           }
 
@@ -362,13 +362,13 @@ export default {
       if(!sail) return
 
       const colors = {
-        "JIB"      : "#cd0342",
-        "SPI"      : "#00ff00",
-        "STAYSAIL"    : "#0000ff",
-        "LIGHT_JIB"    : "#f67876",
-        "CODE_0"    : "#00a000",
-        "HEAVY_GNK"    : "#b00000",
-        "LIGHT_GNK"    : "#d77900"
+        "Jib"      : "#cd0342",
+        "Spi"      : "#00ff00",
+        "Staysail"    : "#0000ff",
+        "LightJib"    : "#f67876",
+        "Code0"    : "#00a000",
+        "HeavyGnk"    : "#b00000",
+        "LightGnk"    : "#d77900"
       }
 
       polar.append("path")
