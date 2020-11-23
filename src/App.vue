ca5bc20e0603286cd3230ea54fb09f3a6be36040
Arnaud Bouts <arnaud.bouts@orange.com>
2020-11-23 16:19:46 +0100

    enable capacitor

--- src/App.vue
+++ src/App.vue
@@ -71,19 +71,21 @@ export default {
       this.boat = this.boats[0].name
     }
 
-    if (Notification.permission === 'granted') {
-      // show notification here
-    } else {
-      // request permission from user
-      Notification.requestPermission().then(function(p) {
-       if(p === 'granted') {
-           // show notification here
-       } else {
-           console.log('User blocked notifications.');
-       }
-      }).catch(function(err) {
-          console.error(err);
-      });
+    if (Notification) {
+      if (Notification.permission === 'granted') {
+        // show notification here
+      } else {
+        // request permission from user
+        Notification.requestPermission().then(function(p) {
+         if(p === 'granted') {
+             // show notification here
+         } else {
+             console.log('User blocked notifications.');
+         }
+        }).catch(function(err) {
+            console.error(err);
+        });
+      }
     }
   },
 }
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,25 @@
 module.exports = {
   devServer: {
     // proxy: {
+    //   // '^/debug/nav/boatlines': {
+    //   //   //target: 'http://localhost:8888'
+    //   //   target: 'https://nav.apps-k3s.no-cloud.fr'
+    //   // },
     //   '^/debug': {
     //     target: 'http://localhost:8888'
     //   },
+    //   '^/private': {
+    //     target: 'http://localhost:8888'
+    //   },
     //   '^/winds': {
-    //     //target: 'http://localhost:8090'
-    //     target: 'https://nav.apps-k3s.no-cloud.fr'
+    //     target: 'http://localhost:8090'
+    //     //target: 'https://nav.apps-k3s.no-cloud.fr'
     //   },
     //   '^/polars': {
-    //     //target: 'http://localhost:7777'
+    //     // target: 'http://localhost:7777'
     //     target: 'https://nav.apps-k3s.no-cloud.fr'
     //   }
     // }
-    proxy: 'https://nav.apps-k3s.no-cloud.fr'
+    proxy: 'https://route.phtheirichthys.fr'
   }
 }
